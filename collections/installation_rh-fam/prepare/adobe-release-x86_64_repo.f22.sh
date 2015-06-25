8986c6ea97129aca94e58f9db090398af0ebcae1
Michael Voetter <Michael.Voetter@student.uibk.ac.at>
2015-06-25 17:37:27 +0200

    add adobe-release repo for f22

---
+++ collections/installation_rh-fam/prepare/adobe-release-x86_64_repo.f22.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+#
+# prepare: this
+# job: none
+# finalize: none
+#
+rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
